a8fd706f0dca3319d7defe3046c934277613bf73
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-14 17:33:56 +0100

    abstracted some helpers

--- helpers/depth.js
+++ helpers/depth.js
@@ -1,38 +1,38 @@
 module.exports = function(handlebars){
     handlebars.registerHelper("heading-depth", function(options){
-        return options.data.root._headingDepth + (options.data.root.argv["heading-depth"]);
+        return options.data.root.options._headingDepth + (options.data.root.options["heading-depth"]);
     });
 
     handlebars.registerHelper("heading-depth-set", function(depth, options){
-        options.data.root._headingDepth = depth;
+        options.data.root.options._headingDepth = depth;
     });
 
     handlebars.registerHelper("heading-depth-reset", function(options){
-        options.data.root._headingDepth = 0;
+        options.data.root.options._headingDepth = 0;
     });
 
     handlebars.registerHelper("heading-depth-bump", function(options){
-        options.data.root._headingDepth++;
+        options.data.root.options._headingDepth++;
     });
 
     handlebars.registerHelper("heading-depth-drop", function(options){
-        options.data.root._headingDepth--;
+        options.data.root.options._headingDepth--;
     });
     
     handlebars.registerHelper("index-depth", function(options){
-        return options.data.root._indexDepth;
+        return options.data.root.options._indexDepth;
     });
 
     handlebars.registerHelper("index-depth-reset", function(options){
-        options.data.root._indexDepth = 0;
+        options.data.root.options._indexDepth = 0;
     });
 
     handlebars.registerHelper("index-depth-bump", function(options){
-        options.data.root._indexDepth++;
+        options.data.root.options._indexDepth++;
     });
 
     handlebars.registerHelper("index-depth-drop", function(options){
-        options.data.root._indexDepth--;
+        options.data.root.options._indexDepth--;
     });
 
 };
