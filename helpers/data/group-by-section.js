0367fbbaf444a2546a3ed0d92e258891a27ce87a
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-07-20 10:56:08 +0100

    refactoring

---
+++ helpers/data/group-by-section.js
@@ -0,0 +1,18 @@
+var a = require("array-tools");
+
+module.exports = function(handlebars){
+    handlebars.registerHelper("groupBySection", function(data){
+        if(!Array.isArray(data)) throw new Error("[groupBySection] requires an array as input");
+        var output = [];
+        output = output.concat(a.extract(data, { kind: "module" }));
+        output = output.concat(a.extract(data, { kind: "class" }));
+        output = output.concat(a.extract(data, { kind: "namespace" }));
+        output = output.concat(a.extract(data, { kind: "event" }));
+        output = output.concat(a.extract(data, { kind: "function" }));
+        output = output.concat(a.extract(data, { kind: "member" }));
+        output = output.concat(a.extract(data, { kind: "typedef" }));
+        output = output.concat(a.extract(data, { kind: "constant" }));
+        output = output.concat(data);
+        return output;
+    });
+};
