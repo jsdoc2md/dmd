b3c354730055d5ceaa4b694f95236fb58abb7eee
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-08-15 15:13:01 +0200

    fixes an error caused by not specifying a @param type #7

--- helpers/link-to.js
+++ helpers/link-to.js
@@ -1,9 +1,11 @@
 var a = require("array-tools"),
-    url = require('url'),
+    url = require("url"),
     util = require("util");
 
 module.exports = function(handlebars){
     handlebars.registerHelper("linkTo", function linkTo(longname, options){
+        if (!longname) return "";
+
         var re = /<(.*)>/;
         var fullName = "";
         if (Array.isArray(longname)){
@@ -15,11 +17,11 @@ module.exports = function(handlebars){
                 fullName = longname;
                 longname = longname.match(re)[1];
             }
-            
+
             var builtInType = /^(string|object|number|boolean|array|regexp|date)$/i.test(longname);
-            
+
             if (builtInType){
-                return options.hash.style !== 'plain' ? "`" + (fullName || longname) + "`" : fullName || longname;
+                return options.hash.style !== "plain" ? "`" + (fullName || longname) + "`" : fullName || longname;
             } else {
                 var linked = a.findWhere(options.data.root, { longname: longname }),
                     mask;
@@ -27,21 +29,21 @@ module.exports = function(handlebars){
                     linked.isConstructor = false;
                     if (fullName) fullName = fullName.replace(/</g, "&lt;").replace(/>/g, "&gt;");
                     var linkText = fullName ? fullName.replace(longname, linked.name) : linked.name;
-                    mask = options.hash.style === 'code' ? "<code>[%s](#%s)</code>" : "[%s](#%s)";
+                    mask = options.hash.style === "code" ? "<code>[%s](#%s)</code>" : "[%s](#%s)";
                     return util.format(mask, linkText, handlebars.helpers.anchorName.call(linked, options));
                 } else {
                     if (url.parse(fullName || longname).protocol) {
                         switch (options.hash.style) {
-                            case 'code':
-                                mask = '<code>[%s](%s)</code>';
+                            case "code":
+                                mask = "<code>[%s](%s)</code>";
                                 break;
-                            case 'plain':
+                            case "plain":
                             default:
-                                mask = '[%s](%s)';
+                                mask = "[%s](%s)";
                         }
                         return util.format(mask, options.hash.caption || fullName || longname, fullName || longname);
                     }
-                    return options.hash.style !== 'plain' ? "`" + (fullName || longname) + "`" : fullName || longname;
+                    return options.hash.style !== "plain" ? "`" + (fullName || longname) + "`" : fullName || longname;
                 }
             }
         }
