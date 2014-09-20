e4de8f22fb8bcacd96dc7cc6a7b52ddfaff4f5c3
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-09-20 20:18:28 +0100

    wip

--- helpers2/vars.js
+++ helpers2/vars.js
@@ -1,16 +1,22 @@
 "use strict";
 
 exports.headingDepth = headingDepth;
-exports.headingDepthIncrement = headingDepthIncrement;
-exports.headingDepthDecrement = headingDepthDecrement;
+exports.depth = depth;
+exports.depthIncrement = depthIncrement;
+exports.depthDecrement = depthDecrement;
 
 function headingDepth(options){
-    return options.data.root.options._headingDepth + (options.data.root.options["heading-depth"]);
-}
-function headingDepthIncrement(options){
-    options.data.root.options._headingDepth++;
+    return options.data.root.options._depth + (options.data.root.options["heading-depth"]);
 }
 
-function headingDepthDecrement(options){
-    options.data.root.options._headingDepth--;
+function depth(options){
+    return options.data.root.options._depth;
+}
+
+function depthIncrement(options){
+    options.data.root.options._depth++;
+}
+
+function depthDecrement(options){
+    options.data.root.options._depth--;
 }
---
+++ test/md/everything.md
@@ -0,0 +1,39 @@
+##Modules
+* module: cjs/class
+  * class: ExportedClass ⏏
+    * new ExportedClass()
+    * exportedClass.prop
+    * ExportedClass.staticProp
+    * ExportedClass~innerProp
+* module: functionAlias
+  * pointerFunction() ⏏
+* module: cjs/function
+  * $~prop
+* module: teams
+  * teams ⏏
+* module: cjs/object
+  * obj.one
+  * obj.two()
+
+##Globals
+* class: GlobalClass
+  * new GlobalClass(one, two)
+  * globalClass.propOne
+  * GlobalClass.propTwo
+* globalFunction(one, two)
+* Eventful
+  * Eventful.on(eventName, handler)
+  * Eventful.fire(eventName, eventData)
+* globalVar
+
+
+
+
+
+
+
+
+
+
+
+
