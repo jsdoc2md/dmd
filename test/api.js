98d83773935730424a7f12c011c6c4519fd98525
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-03-14 15:12:58 +0000

    plugin test

--- test/api.js
+++ test/api.js
@@ -1,13 +1,13 @@
 "use strict";
 var test = require("tape");
 var dmd = require("../");
-var fs = require("fs");
 
 test("dmd() on readable returns correct data", function(t){
     t.plan(1);
-    
-    fs.createReadStream("test/fixture/everything.json").pipe(dmd()).on("readable", function(){
+    var dmdStream = dmd();
+    dmdStream.on("readable", function(){
         var md = this.read();
-        if (md) t.ok(/exports a class/.test(md.toString()));
+        if (md) t.ok(/is a class/.test(md.toString()));
     });
+    dmdStream.end('[ { "id": "someclass", "longname": "someclass", "name": "someclass", "kind": "class", "description": "is a class" } ]');
 });
--- test/plugin.js
+++ test/plugin.js
@@ -2,3 +2,13 @@
 var test = require("tape");
 var dmd = require("../");
 
+test("plugin", function(t){
+    t.plan(1);
+    
+    var dmdStream = dmd({ plugin: "dmd-plugin-example" });
+    dmdStream.on("readable", function(){
+        var md = this.read();
+        if (md) t.ok(/documentation generated on/.test(md.toString()));
+    });
+    dmdStream.end('[ { "id": "someclass", "longname": "someclass", "name": "someclass", "kind": "class", "description": "is a class" } ]');    
+});
