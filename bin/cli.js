c83026789110f31b8fe3bbf813ecff922ab4e475
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-05-16 15:23:24 +0100

    added stored config and --config option.. added/upgraded deps

--- bin/cli.js
+++ bin/cli.js
@@ -5,15 +5,21 @@ var ansi = require("ansi-escape-sequences");
 var dmd = require("../");
 var domain = require("domain");
 var fs = require("fs");
+var loadConfig = require("config-master");
+var homePath = require("home-path");
+var path = require("path");
+var o = require("object-tools");
 
 var cli = cliArgs(dmd.cliOptions.concat([
     { name: "help", alias: "h", type: Boolean },
-    { name: "verbose", alias: "v", type: Boolean }
+    { name: "verbose", alias: "v", type: Boolean },
+    { name: "config", type: Boolean, description: "Print the stored config and exit" }
 ]));
 
 var usage = cli.getUsage({
     title: "dmd",
     header: "Generate markdown API documentation",
+    footer: "\n  Project home: https://github.com/jsdoc2md/dmd",
     forms: [
         "$ cat jsdoc-parse-output.json | dmd [<options>]"
     ]
@@ -25,13 +31,26 @@ try{
     halt(err);
 }
 
-if (argv.help){
+var dmdConfig = loadConfig(
+    path.join(homePath(), ".dmd.json"),
+    path.join(process.cwd(), ".dmd.json"),
+    { jsonPath: path.join(process.cwd(), "package.json"), configProperty: "dmd" }
+);
+
+var config = o.extend(dmdConfig, argv);
+
+if (config.config){
+    console.log(JSON.stringify(o.without(config, "config"), null, "  "));
+    process.exit(0);
+}
+
+if (config.help){
     console.log(usage);
     process.exit(0);
 }
 
-if (argv.template){
-    argv.template = fs.readFileSync(argv.template, "utf8");
+if (config.template){
+    config.template = fs.readFileSync(config.template, "utf8");
 }
 
 var dmdStream = dmd(argv);
@@ -41,9 +60,9 @@ process.stdin.pipe(dmdStream).pipe(process.stdout);
 
 function halt(err){
     if (err.code === "EPIPE") process.exit(0); /* no big deal */
-    
+
     if (argv){
-        if (argv.verbose){
+        if (config.verbose){
             logError("Error: " + err.message);
             logError(err.stack || err);
         } else {
