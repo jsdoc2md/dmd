469300f829b173e6a990e70ad57382bd8bd9dd5f
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-01-10 22:31:22 +0000

    tree

--- bin/cli.js
+++ bin/cli.js
@@ -9,21 +9,26 @@ let template = markdownParamList
 // let template = terminal
 // let template = markdown
 
-const dataTreeBuilder = require('../lib/doclet-data-builder')
+const dataTreeBuilder = require('../lib/builder-doclet-data')
+const templateTreeBuilder = require('../lib/builder-doclet-template')
 
 const doclets = require('../data/handbrake-js')
 const docletTree = dataTreeBuilder(doclets)
 
 // console.log(require('util').inspect(docletTree, { depth: null }));
-for (let doclet of docletTree) {
-  console.log(doclet.id)
+// for (let doclet of docletTree) {
+//   console.log(doclet.id)
+// }
+
+
+const docs = templateTreeBuilder(docletTree)
+// console.log(require('util').inspect(docs, { depth: null }));
+for (let doclet of docs) {
+  console.log(doclet.signature)
 }
 return
-
-const docs = templateTreeBuilder.build(docletTree)
-
 const fs = require('fs')
-const templateString = fs.readFileSync('./output-template/simple.md', 'utf8')
+const templateString = fs.readFileSync('./output-template/tree.md', 'utf8')
 
 /* de-couple so templating method is passed in */
 const result = eval('`' + templateString + '`')
