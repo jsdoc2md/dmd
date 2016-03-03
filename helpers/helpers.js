6f71f69f2bcd724ea6a913c9b5e9137dc0033663
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-03-03 12:46:12 +0000

    deps.. fix regression

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -281,7 +281,7 @@ function params (options) {
   if (this.params) {
     var list = this.params.map(function (param) {
       var nameSplit = param.name.split('.')
-      var name = a(nameSplit).last()
+      var name = nameSplit[nameSplit.length - 1]
       if (nameSplit.length > 1) name = '.' + name
       if (param.variable) name = '...' + name
       if (param.optional) name = '[' + name + ']'
